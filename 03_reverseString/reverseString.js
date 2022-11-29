0375f62beef28d6f83ada7eb2537320001ad2484
Tanorico <aaron.m.elias@gmail.com>
2022-11-29 20:24:05 +1100

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const tempArray = string.split("");
+  const doneString = [];
+  for (i = string.length; i >= 0; i--){
+    doneString.push(tempArray[i]);
+  }
+  return doneString.join("");
 };
 
 // Do not edit below this line
