45780857838b9d6d2128e7a9dcc1a720dabd28f7
Tanorico <aaron.m.elias@gmail.com>
2022-11-30 20:49:08 +1100

    Complete Exercise 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
 const removeFromArray = function() {
-
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
+    let testArray = arguments[0];
+    let returnArray = [];
+    
+    for (i = 0; i < testArray.length; i++){
+      let deleteChar = false; 
+      for (j = 1; j < arguments.length; j++){
+          if (testArray[i] === arguments[j]){
+              deleteChar = true;
+          }    
+      }
+      if (!deleteChar){
+          returnArray.push(testArray[i]);
+      }
+    }
+  
+  
+    return returnArray;
+  };
+  
+  console.log(removeFromArray([1, 2, 23, 324, 4], 324,1));
+  
+  // Do not edit below this line
+  module.exports = removeFromArray;
